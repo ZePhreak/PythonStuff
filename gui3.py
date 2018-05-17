7dba88282a12129df83db465c5760fbef6bb19ff
Brandon <f43ak@msn.com>
2018-05-17 15:50:23 -0400

    Refactor to the limit

--- gui3.py
+++ gui3.py
@@ -1,5 +1,7 @@
 #MainImports
-
+import func
+import var
+import statext
 #
 from tkinter import *
 from tkinter import ttk
@@ -24,12 +26,6 @@ from matplotlib.figure import Figure
 import re
 from decimal import *
 mpl.use('TkAgg')
-#Import my functions
-import func as func
-import var as var
-
-#Globals
-
 
 #Variables
 stats = [ "Determination", "Crit", "Direct Hit" ]
@@ -76,12 +72,30 @@ nb.grid(row=1, column=0, columnspan=50, rowspan=49, sticky='NESW')
 
 #Update Function
 def callback(number):
-		import var
-		if 1:
-			func.loadstats()
-			lbl4.configure(text = var.p_det)
-			lbl5.configure(text = var.p_direct)
-			lbl6.configure(text = var.p_crit)
+	import var
+	if number == 1:
+		print('Load Stats Called')
+		func.loadstats()
+		lbl4.configure(text = var.p_det)
+		lbl5.configure(text = var.p_direct)
+		lbl6.configure(text = var.p_crit)
+	if number == 2:
+		try:
+			print('Stat Extrapolate Called')
+			statext.stat_extrapolate()
+			statinfo1.configure(text = var.stat_info1)
+			statinfo2.configure(text = var.stat_extra1)
+			statinfo3.configure(text = var.stat_extra2)
+			statinfo4.configure(text = var.stat_info2)
+			statinfo5.configure(text = var.stat_extra3)
+			statinfo6.configure(text = var.stat_extra4)
+			statinfo7.configure(text = var.stat_info3)
+			statinfo8.configure(text = var.stat_extra5)
+			statinfo9.configure(text = var.stat_extra6)
+		except:
+			pass
+	if number == 3:
+		pass
 
 
 
@@ -305,97 +319,7 @@ Checkbutton(page2, text="Direct", variable=showDirect, command=stat_state2).plac
 Checkbutton(page2, text="Tenacity", variable=showTen, command=stat_state3).place(x=780, y=260)
 Checkbutton(page2, text="S Speed", variable=showS, command=stat_state4).place(x=780, y=280)
 
-#Stat Extrapolation
 
-def stat_extrapolate():
-	import var
-	#Variables
-	stat_flags = [True, True, True]
-	statinfo1.configure(text = 'Input Stats')
-	statinfo2.configure(text = 'Input Stats')
-	statinfo3.configure(text = 'Input Stats')
-	statinfo4.configure(text = 'Input Stats')
-	statinfo5.configure(text = 'Input Stats')
-	statinfo6.configure(text = 'Input Stats')
-	statinfo7.configure(text = 'Input Stats')
-	statinfo8.configure(text = 'Input Stats')
-	statinfo9.configure(text = 'Input Stats')
-	i = 0
-	#p_det = int(time.strftime(e1.get()))
-	#p_direct = int(time.strftime(e2.get()))
-	#p_crit = int(time.strftime(e3.get()))
-	#det_request = int(time.strftime(e4.get()))
-	#direct_request = int(time.strftime(e5.get()))
-	#crit_request = int(time.strftime(e6.get()))
-	#request6 = int(time.strftime(e7.get()))
-	x = floor(float(var.request6) / 40 ) * 40
-	while not stat_flags == [False, False, False]:
-			print(var.det_request)
-			print(var.request6)
-		#Setting some variables
-			det_array = [0,0]
-			crit_array = [0,0]
-			direct_array = [0,0]
-	#Search by player stats + max possible stat as long as it doesn't exceed max possible meld if state = true
-			if stat_flags[0]:
-				det_data1 = np.searchsorted(np.squeeze(datafile[0][0]), (float(var.p_det) + min(int(var.det_request), x)), side='left')
-				det_array = datafile[0][:, det_data1-1]
-			if stat_flags[1]:
-				crit_data1 = np.searchsorted(np.squeeze(datafile[1][0]), (float(var.p_crit) + min(int(var.crit_request), x)), side='left')
-				crit_array = datafile[1][:, crit_data1-1]
-			if stat_flags[2]:
-				direct_data1 = np.searchsorted(np.squeeze(datafile[2][0]), (float(var.p_direct) + min(int(var.direct_request), x)), side='left')
-				direct_array = datafile[2][:, direct_data1-1]
-	#Find the highest of the searches and mark it as found
-			mult_data = np.array( [det_array, crit_array, direct_array] ).T
-			bestmults = mult_data[1]
-			beststat_ind = np.argmax(bestmults)
-			beststat_val = mult_data[0][beststat_ind]
-			beststat_remainder = floor(beststat_ind / 40 ) * 40
-	#Update Variables
-			stat_flags[beststat_ind] = False
-			if beststat_ind == 0:
-				x = x - min(float(var.det_request), x)
-				print(x)
-			if beststat_ind == 1:
-				x = x - min(float(var.crit_request), x)
-				print(x)
-			if beststat_ind == 2:
-				x = x - min(float(var.direct_request), x)
-				print(x)
-	#Separate each runs output into separate variables
-			if i == 0:
-				global stat_extra1
-				global stat_extra2
-				global stat_info1
-				stat_extra1 = bestmults[beststat_ind]
-				stat_extra2 = beststat_val
-				stat_info1 = stats[beststat_ind]
-				statinfo1.configure(text = stat_info1)
-				statinfo2.configure(text = stat_extra1)
-				statinfo3.configure(text = stat_extra2)
-			if i == 1:
-				global stat_extra3
-				global stat_extra4
-				global stat_info2
-				stat_extra3 = bestmults[beststat_ind]
-				stat_extra4 = beststat_val
-				stat_info2 = stats[beststat_ind]
-				statinfo4.configure(text = stat_info2)
-				statinfo5.configure(text = stat_extra3)
-				statinfo6.configure(text = stat_extra4)
-			if i == 2:
-				global stat_extra5
-				global stat_extra6
-				global stat_info3
-				stat_extra5 = bestmults[beststat_ind]
-				stat_extra6 = beststat_val
-				stat_info3 = stats[beststat_ind]
-				statinfo7.configure(text = stat_info3)
-				statinfo8.configure(text = stat_extra5)
-				statinfo9.configure(text = stat_extra6)
-				scatter()
-			i = i + 1
 
 def scatter():
 					d1 = (stat_extra2, stat_extra1)
@@ -443,7 +367,7 @@ def scatter():
 
 
 
-button2 = Button(page2, text="Calculate", command = stat_extrapolate)
+button2 = Button(page2, text="Calculate", command = lambda number = 2: callback(number) )
 #button2.place(x=380, y=90)
 button2.grid(column=4, row= 4)
 
@@ -452,7 +376,7 @@ button2.grid(column=4, row= 4)
 button3 = Button(page1, text="Save", command = func.savestats)
 button3.place(x=150, y=68)
 
-button4 = Button(page1, text="Load", command =lambda: callback(1))
+button4 = Button(page1, text="Load", command =lambda number = 1 : callback(number))
 button4.place(x=125, y=98)
 
 ######################################## Tab 3 - Gear Comparison ########################################
--- statext.py
+++ statext.py
@@ -1,22 +1,24 @@
-import var
 import numpy as np
+from math import *
+stats = [ "Determination", "Crit", "Direct Hit" ]
 
 #Load CSVS
 data_list = ['det3.csv','crit.csv','direct.csv','tenacity.csv','ss.csv']
 datafile = [np.loadtxt(file, delimiter=",") for file in data_list]
 
 
-
 def stat_extrapolate():
+	import var
 	#Variables#
 	stat_flags = [True, True, True]
-	i = 0
+	r = 0
 	x = floor(float(var.request6) / 40 ) * 40
 	det_array = [0,0]
 	crit_array = [0,0]
 	direct_array = [0,0]
 	#Variables#
-	while not stat_flags == [False, False, False]:
+	#while not stat_flags == [False, False, False]:
+	for i in range(3):
 	#Search by player stats + max possible stat as long as it doesn't exceed max possible meld if state = true
 			if stat_flags[0]:
 				det_data1 = np.searchsorted(np.squeeze(datafile[0][0]), (float(var.p_det) + min(int(var.det_request), x)), side='left')
@@ -36,54 +38,37 @@ def stat_extrapolate():
 	#Update Variables
 			stat_flags[beststat_ind] = False
 			if beststat_ind == 0:
-				x = x - min(float(var.det_request), x)
-				print(x)
+				x = x - min(var.det_request, x)
+				print("0 done")
 			if beststat_ind == 1:
-				x = x - min(float(var.crit_request), x)
-				print(x)
+				x = x - min(var.crit_request, x)
+				print("1 done")
 			if beststat_ind == 2:
-				x = x - min(float(var.direct_request), x)
-				print(x)
+				x = x - min(var.direct_request, x)
+				print("2 done")
 	#Separate each runs output into separate variables
-			if i == 0:
-				global stat_extra1
-				global stat_extra2
-				global stat_info1
+			if r == 0:
 				stat_extra1 = bestmults[beststat_ind]
 				stat_extra2 = beststat_val
 				stat_info1 = stats[beststat_ind]
 				var.stat_info1 = stat_info1
 				var.stat_extra1 = stat_extra1
 				var.stat_extra2 = stat_extra2
-				stat+extra 2
-				statinfo1.configure(text = stat_info1)
-				statinfo2.configure(text = stat_extra1)
-				statinfo3.configure(text = stat_extra2)
-			if i == 1:
-				global stat_extra3
-				global stat_extra4
-				global stat_info2
+			if r == 1:
 				stat_extra3 = bestmults[beststat_ind]
 				stat_extra4 = beststat_val
 				stat_info2 = stats[beststat_ind]
-				statinfo4.configure(text = stat_info2)
-				statinfo5.configure(text = stat_extra3)
-				statinfo6.configure(text = stat_extra4)
 				var.stat_info2 = stat_info2
 				var.stat_extra3 = stat_extra3
 				var.stat_extra4 = stat_extra4
-			if i == 2:
-				global stat_extra5
-				global stat_extra6
-				global stat_info3
+			if r == 2:
 				stat_extra5 = bestmults[beststat_ind]
 				stat_extra6 = beststat_val
 				stat_info3 = stats[beststat_ind]
-				statinfo7.configure(text = stat_info3)
-				statinfo8.configure(text = stat_extra5)
-				statinfo9.configure(text = stat_extra6)
 				var.stat_info3 = stat_info3
 				var.stat_extra5 = stat_extra5
 				var.stat_extra6 = stat_extra6
-				scatter()
-			i = i + 1
+				print("noot")
+				#scatter()                  # Re add soon
+			r = r + 1
+			print(r)
